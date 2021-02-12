45c6f4fec1956e65b05ba714c2d5dc30c62edde8
zhangtao615 <295564283@qq.com>
2021-02-12 16:10:44 +0800

    '加密'

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,9 +1,13 @@
 const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 
 const login = (username, password) => {
   // 使用escape后拼接sql语句需要加引号
   let username = escape(username)
-  let password = escape(password)
+   
+  // 生成加密密码
+  let password = genPassword(password)
+  password = escape(password)
   const sql = `
     select username, realname from t_users where username=${username} and password=${password};
   `
